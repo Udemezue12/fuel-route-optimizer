bb164ce8a1a1e22a8e1613b2d1f34c87e57cfd1b
Uchechukwu Udemezue <udemezue008@gmail.com>
2025-08-01 01:55:50 +0200

    Modifications were made

--- fuel_project/settings.py
+++ fuel_project/settings.py
@@ -143,13 +143,13 @@ CACHES = {
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://fuel-route-optimizer.onrender.com", "fuel-route-optimizer.onrender.com",
+    "https://fuel-route-optimizer.onrender.com", 
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://fuel-route-optimizer.onrender.com", "fuel-route-optimizer.onrender.com"]
+    "https://fuel-route-optimizer.onrender.com"]
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 # for production
