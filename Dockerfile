c1e1b24dae6f4a48fc41a84664af9a07790bc5d5
Uchechukwu Udemezue <udemezue008@gmail.com>
2025-08-01 01:20:15 +0200

    Modifications were made

--- Dockerfile
+++ Dockerfile
@@ -19,9 +19,6 @@ COPY . /app
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-# Collect static files
-RUN python manage.py collectstatic --noinput
-
 # Add Supervisor config
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ echo "▶️ Applying Django migrations..."
 python manage.py makemigrations
 python manage.py migrate
 echo "✅ Migrations done."
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 echo "▶️ Starting Supervisor to manage Uvicorn and Celery..."
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
